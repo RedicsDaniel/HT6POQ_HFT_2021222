cea3d55fa8ecd57d9cefc1ee7358f426f6dad4a0
RedicsDaniel <redicsd@gmail.com>
2022-03-25 17:59:19 +0100

    projects added

---
+++ HT6POQ_HFT_2021222.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HT6POQ_HFT_2021222.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ HT6POQ_HTF_2021222.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HT6POQ_HTF_2021222.Models
+{
+    public class Class1
+    {
+    }
+}
