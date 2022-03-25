4c9411addd75b75291548564e8cef9d7ba1fd536
RedicsDaniel <redicsd@gmail.com>
2022-03-25 17:49:44 +0100

    Add project files.

---
+++ HT6POQ_HFT_2021222/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HT6POQ_HFT_2021222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
