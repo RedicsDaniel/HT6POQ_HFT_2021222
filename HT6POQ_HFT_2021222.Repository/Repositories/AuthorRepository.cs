e45ddde8ef8ac74f519a65d6fd01026820fc28e4
RedicsDaniel <redicsd@gmail.com>
2022-03-31 18:33:38 +0200

    shop and author done

---
+++ HT6POQ_HFT_2021222.Repository/Repositories/AuthorRepository.cs
@@ -0,0 +1,53 @@
+﻿using HT6POQ_HFT_2021222.Repository.Data;
+using HT6POQ_HFT_2021222.Repository.Interfaces;
+using HT6POQ_HTF_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT6POQ_HFT_2021222.Repository.Repositories
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        BookDbContext db;
+        public AuthorRepository(BookDbContext DB)
+        {
+            this.db = DB;
+        }
+        public void Create(Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var AuthorToDelete = Read(id);
+            db.Authors.Remove(AuthorToDelete);
+            db.SaveChanges();
+        }
+
+        public IQueryable<Author> GetAll()
+        {
+            return db.Authors;
+        }
+
+        public Author Read(int id)
+        {
+            return db.Authors.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void Update(Author author)
+        {
+            var NewAuthor = Read(author.Id);
+            NewAuthor.Name = author.Name;
+            NewAuthor.Nationality = author.Nationality;
+            NewAuthor.NumberOfBooks = author.NumberOfBooks;
+            db.SaveChanges();
+
+
+        }
+    }
+}
---
+++ HT6POQ_HFT_2021222.Repository/Repositories/ShopRepository.cs
@@ -0,0 +1,51 @@
+﻿using HT6POQ_HFT_2021222.Repository.Data;
+using HT6POQ_HFT_2021222.Repository.Interfaces;
+using HT6POQ_HTF_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT6POQ_HFT_2021222.Repository.Repositories
+{
+    public class ShopRepository : IShopRepository
+    {
+        BookDbContext db;
+        public ShopRepository(
+            BookDbContext DB)
+        {
+            this.db = DB;
+        }
+        public void Create(Shop shop)
+        {
+            db.Shops.Add(shop);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var ShopToDelete = Read(id);
+            db.Shops.Remove(ShopToDelete);
+            db.SaveChanges();
+        }
+
+        public IQueryable<Shop> GetAll()
+        {
+            return db.Shops;
+        }
+
+        public Shop Read(int id)
+        {
+            return db.Shops.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void Update(Shop shop)
+        {
+            var NewShop = Read(shop.Id);
+            NewShop.Location = shop.Location;
+            NewShop.Name = shop.Name;
+            db.SaveChanges();
+        }
+    }
+}
