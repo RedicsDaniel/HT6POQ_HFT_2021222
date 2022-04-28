7821afceaee1d8af18acc6070905301642243eeb
RedicsDaniel <redicsd@gmail.com>
2022-04-28 14:45:12 +0200

    interfaces created

---
+++ HT6POQ_HFT_2021222.Logic/Interfaces/IShopLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT6POQ_HFT_2021222.Logic.Interfaces
+{
+    public interface IShopLogic
+    {
+    }
+}
