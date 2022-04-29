1f55ee20f36af3517b0510cc43fdf7805b14e437
RedicsDaniel <redicsd@gmail.com>
2022-04-29 23:30:50 +0200

    restservice

--- HT6POQ_HFT_2021222/Program.cs
+++ HT6POQ_HFT_2021222/Program.cs
@@ -1,16 +1,32 @@
-﻿using HT6POQ_HFT_2021222.Repository.Data;
+﻿
+using ConsoleTools;
+using HT6POQ_HTF_2021222.Models;
 using System;
 using System.Linq;
 
 namespace HT6POQ_HFT_2021222
 {
-    class Program
+    internal class Program
     {
+       
         static void Main(string[] args)
         {
-            BookDbContext db = new BookDbContext();
-           
-            ;
+             System.Threading.Thread.Sleep(5000);
+             RestService service = new RestService("http://localhost:60996/","book");
+               
+
+            
+        }
+        static void Option(RestService service)
+        {
+            Console.WriteLine(" 1 : Book table ");
+            Console.WriteLine(" 2 : Author table ");
+            Console.WriteLine(" 3 : Shop table ");
+            Console.WriteLine(" 4 : Book CRUD ");
+            Console.WriteLine(" 5 : Author CRUD ");
+            Console.WriteLine(" 6 : Shop CRUD ");
+            Console.WriteLine(" 7 : Book NON-CRUD ");
+            Console.WriteLine(" 8 : Author NON-CRUD ");
         }
     }
 }
